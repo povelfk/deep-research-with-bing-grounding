9f05535defef193b252af60341a1178630ad7196
Povel Forsare Källman <64473024+povelfk@users.noreply.github.com>
2025-06-05 23:02:49 +0200

    adding invoke_agent method

--- common/utils_ai_agents.py
+++ common/utils_ai_agents.py
@@ -15,6 +15,15 @@ def get_last_message_by_role(project_client, thread_id, role):
     Retrieves the last message from an agent in a specified thread.
     """
     return project_client.agents.messages.get_last_message_by_role(
-    thread_id=thread_id,
-    role=MessageRole.AGENT
-).content[0]['text']['value']
+        thread_id=thread_id,
+        role=MessageRole.AGENT
+    ).content[0]['text']['value']
+
+
+def invoke_agent(project_client, thread, agent):
+    run = project_client.agents.runs.create_and_process(thread_id=thread.id, agent_id=agent.id)
+    # print(f"run status: {run.status}")
+
+    # get the last message from the planner agent
+    last_agent_output = get_last_message_by_role(project_client, thread.id, MessageRole.AGENT)
+    return last_agent_output, thread
