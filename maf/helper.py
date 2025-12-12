1bba564277c01cf4860a600b2d0d77da50a939fd
Povel Forsare Källman <64473024+povelfk@users.noreply.github.com>
2025-12-12 11:32:32 +0100

    fix: making sure reports folder is created if not exist

--- maf/helper.py
+++ maf/helper.py
@@ -1,7 +1,11 @@
 import datetime
+import os
 
 def save_report(report):
     """Saves the final report to a markdown file."""
+
+    # Ensure reports directory exists
+    os.makedirs("reports", exist_ok=True)
 
     current_datatime = datetime.datetime.now().strftime("%Y-%m-%d-%H%M%S")
     with open(f"reports/report_{current_datatime}.md", "w", encoding="utf-8") as f:
